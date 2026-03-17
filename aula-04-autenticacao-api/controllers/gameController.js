560c525aa768ccd674ecfaf713ec9057b2fc0ec2
ricardosugano <sugano.ricardo@gmail.com>
2026-03-17 19:24:48 -0300

    2

---
+++ aula-04-autenticacao-api/controllers/gameController.js
@@ -0,0 +1,65 @@
+//importando o service
+import gameService from "../services/gameService.js";
+//importando ObjectId do mongodb
+import { ObjectId } from 'mongodb';
+
+//Funçao para tratar a requisiçao de listar os jogos
+const getAllGames = async (req, res) =>{
+    try{
+        const games = await gameService.getAll()
+        res.status(200).json({games : games}) // cod.200 : Requisição feita com sucesso
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error : 'Erro interno do servidor'})
+    }
+}
+
+// função para tratar a requiasição de CADASTRAR um jogo
+const createGame = async(req, res) => {
+    try{
+        //Desestruturação
+        // coletadno os dados do corpo da requisição
+        const {title, year, price, descriptions} = req.body
+        await gameService.Create(title, year, price, descriptions)
+        //res.sendStatus(201) - usado para enviar apenas o status
+        res.status(201).json({message: 'O jogo foi cadastrado com sucesso!'})
+        // cod. 201 - CREATE - Um novo recurso foi criado no servidor
+    }catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Erro interno do servidor. Não foi possivel cadastrar o jogo'})
+    }
+}
+
+// função para tratar a requisição de DELETAR um jogo
+const deleteGame = async (req, res) => {
+    try{
+        const id = req.params.id
+        if (ObjectId.isValid(id)){
+            await gameService.delete(id)
+            res.status(204).json({message : 'O jogo foi escluido com sucesso'})
+        }else{
+            res.status(400).json({ error: 'Ocorreu um erro na validação da ID'})
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error: 'Erro interno do servidor. Não foi possível deletar o jogo'})
+    }
+}
+
+// função para tratar a requisição de ATUALIZAR um jogo
+const updateGame = async (req, res) => {
+    try{
+        const id = req.params.id
+        if (ObjectId.isValid(id)){
+            const {title, year, price, descriptions} = req.body
+            await gameService.update(id, title, year, price, descriptions)
+            res.status(200).json({message: 'Jogo atualizado com sucesso!'})
+        }else{
+            res.status(404).json({message: 'Jogo não encontrado'})
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error: 'Erro interno do servidor. Não foi possível atualizar o jogo'})
+    }
+}
+export default { getAllGames, createGame, updateGame, deleteGame }
